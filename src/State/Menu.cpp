7be55a5a2ec3fed096f4afb59be65aa0ff659fd3
Hugo Ratiney <hugo.ratiney@gmail.com>
2013-05-18 17:31:24 -0400

    Items du menu apparaissent avec une animation

--- src/State/Menu.cpp
+++ src/State/Menu.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include "State/Menu.hpp"
 #include "Log.hpp"
 
@@ -14,7 +15,8 @@ Menu::Menu(Game& game)
     for (int i=0; i<3; ++i) {
         sf::Text item = game.res.getText("DejaVu.ttf", strs[i], sf::Color::White);
         sf::FloatRect r = item.getLocalBounds();
-        item.setPosition((int) (game.settings.mode.width - r.width)/2, 180 + 60*i);
+        item.setOrigin(r.width/2.f, r.height/2.f);
+        item.setPosition(game.settings.mode.width/2, 180 + 60*i);
         m_items.push_back(item);
     }
 }
@@ -37,7 +39,20 @@ void Menu::update(Game& game, sf::Time frameTime)
 {
     State::update(game, frameTime);
     
+    const int duration = 600;
+    const int offset = 200;
+    const int totalDuration = duration + m_items.size()*200;
     
+    if (getElapsedTime().asMilliseconds() <= totalDuration)
+    {
+        int totalOffset = 0;
+        for (std::vector<sf::Text>::iterator it = m_items.begin(); it != m_items.end(); ++it)
+        {
+            int t = getElapsedTime().asMilliseconds() - totalOffset;
+            it->setScale((t < 0)? 0 : ((t > duration)? 1.f : std::sin(t * (M_PI/2) / duration)), 1.f);
+            totalOffset += offset;
+        }
+    }
 }
 
 void Menu::draw(Game& game) const
